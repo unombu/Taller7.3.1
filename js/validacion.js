b82fd43d5ba469eddba13ace2d132f6d168ee630
unombu <belen.ungari@gmail.com>
2024-08-14 19:07:29 -0300

    pass y term

--- js/validacion.js
+++ js/validacion.js
@@ -7,5 +7,14 @@ function showAlertError() {
 }
 
 document.getElementById("regBtn").addEventListener("click", function(){
-    
+    const name = document.getElementById("nombre").value.trim()
+    const lastName = document.getElementById("apellido").value.trim()
+    const email = document.getElementById("email").value.trim()
+    const pasword = document.getElementById("password1").value.trim()
+    const repPassword = document.getElementById("password2").value.trim()
+    const term = document.getElementById("terminos").checked
+    if ( pasword === repPassword || term.checked )
+        showAlertSuccess();
+    else
+        showAlertError(); 
 })
