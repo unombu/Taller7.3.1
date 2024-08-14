02ea52467aa9c9510df9001a442e2c8af262c73d
unombu <belen.ungari@gmail.com>
2024-08-14 19:22:23 -0300

    ahita

--- js/validacion.js
+++ js/validacion.js
@@ -10,7 +10,7 @@ document.getElementById("regBtn").addEventListener("click", function(){
     const name = document.getElementById("nombre").value.trim()
     const lastName = document.getElementById("apellido").value.trim()
     const email = document.getElementById("email").value.trim()
-    const pasword = document.getElementById("password1").value.trim()
+    const password = document.getElementById("password1").value.trim()
     const repPassword = document.getElementById("password2").value.trim()
     const term = document.getElementById("terminos").checked
     
@@ -18,11 +18,11 @@ document.getElementById("regBtn").addEventListener("click", function(){
         showAlertError()
         return;
         }
-    if ( pasword !== repPassword && !term ){
+    if ( password !== repPassword && !term ){
         showAlertError()
         return;
     }
-    if (password1.length < 6) {
+    if (password.length < 6) {
         showAlertError()
         return;
     }
